d6199a4b69d342498aeefb34a8b8357b4844039b
sagar <sagargurung1001@gmail.com>
2022-09-10 14:54:54 +0545

    Added rocket chat notification

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
 import * as core from "@actions/core";
 // get state of each issue
 import {
-  getIssueState,
-  getAllAvailableGithubIssueLinks,
-  getAllExpectedToFailureFiles
+    getIssueState,
+    getAllAvailableGithubIssueLinks,
+    getAllExpectedToFailureFiles,
+    notifyToRocketChat
 } from "./scripthelper.js";
 import 'dotenv/config'
+
 const apiURL = {
   githubBaseApiURL:
     "https://api.github.com/repos/owncloud/",
   commonEntryPoint: 'https://github.com/owncloud/',
 };
 
+
 const mdFiles = getAllExpectedToFailureFiles(process.env.PATH_TO_EXPECTED_TO_FAILURE || './expectedToFailure')
-//
-mdFiles.forEach((mdFile) => {
-  const ocisIssuesLink = getAllAvailableGithubIssueLinks(mdFile);
-  ocisIssuesLink.forEach(async (link) => {
-      const endPoint = link.replace(apiURL.commonEntryPoint,"")
-      const fullApiURL = apiURL.githubBaseApiURL + endPoint;
-      const state = await getIssueState(fullApiURL);
-      if (state === "closed") {
-          console.log('\x1b[0m', '\x1b[33m', 'Warning !!' +  ' This ocis issue ' + link + ' has been closed. Please Open it and Update Expected to failure File')
-      }
-  });
+
+const checkExpectedToFailureFiles = new Promise(function(resolve, reject) {
+    let hasClosedIssue = false;
+    mdFiles.forEach((mdFile) => {
+        const ocisIssuesLink = getAllAvailableGithubIssueLinks(mdFile);
+        ocisIssuesLink.forEach(async (link) => {
+            const endPoint = link.replace(apiURL.commonEntryPoint,"")
+            const fullApiURL = apiURL.githubBaseApiURL + endPoint;
+            const state = await getIssueState(fullApiURL);
+            if (state === "closed") {
+                hasClosedIssue = true;
+                console.log('\x1b[0m', '\x1b[33m', 'Warning !!' +  ' This ocis issue ' + link + ' has been closed. Please Open it and Update Expected to failure File')
+            }
+        });
+    });
+    // dummy option
+    setTimeout(() => {
+        resolve(hasClosedIssue)
+    }, 5000)
 });
+
+checkExpectedToFailureFiles.then(async (value) => {
+    await notifyToRocketChat(process.env.ROCKETCHAT_SERVER_URL, process.env.ROCKETCHAT_ACCESS_TOKEN, process.env.ROCKETCHAT_USER_ID, value);
+})
 
--- scripthelper.js
+++ scripthelper.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import parseMD from "parse-md";
 import path from 'path'
 import axios from "axios";
+import 'dotenv/config'
 
 // this function returns state of an issue
 export const getIssueState = async (issueUrl) => {
@@ -10,7 +11,7 @@ export const getIssueState = async (issueUrl) => {
       headers: {
         Accept: "application/vnd.github+json",
         // done with personal GitHub token
-        Authorization: "token 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"
+        Authorization: `token ${process.env.ACCESS_TOKEN}`
       },
     })
     if(response !== null){
@@ -50,4 +51,20 @@ export const getAllExpectedToFailureFiles = (folderPath) => {
       }
   )
   return result;
+}
+
+export const notifyToRocketChat = async (serverURL, access_token, user_id, hasClosedIssue) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      "X-Auth-Token": access_token,
+      "X-User-Id": user_id,
+    }
+  }
+  let data = {
+    "channel": "Test-Channel",
+    "text": (hasClosedIssue)? "Warning !!! There are some issue that has been closed but still in Expected to Failure" : "Bravo!!! All the expected to failures files are upto date."
+}
+  const response = await axios.post(serverURL, data, config);
+  console.log(response.data.success)
 }
