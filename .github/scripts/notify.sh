28928097e5b2ae8a09f1ab4ac3d37a5026bf5407
sagargurung1001@gmail.com <sagargurung1001@gmail.com>
2023-11-28 12:53:50 +0545

    Check for notification script to element

---
+++ .github/scripts/notify.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# helper functions
+log_error() {
+  echo -e "\e[31m$1\e[0m"
+}
+
+log_info() {
+  echo -e "\e[37m$1\e[0m"
+}
+
+log_success() {
+  echo -e "\e[32m$1\e[0m"
+}
+
+sudo apt-get install jq -y
+response=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+  "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/runs/$RUN_ID/jobs")
+
+
+if [[ "$response" != *"jobs"* ]]; then
+  log_error "No jobs found in the below response!"
+  log_info "$response"
+  exit 1
+fi
+
+jobs_informations=$(echo "$response" | jq '.jobs[:-1]')
+jobs_conclusions=$(echo "$jobs_informations" | jq -r '.[].conclusion')
+
+workflow_status="Success"
+if [[ " ${jobs_conclusions[*]} " == *"failure"* ]]; then
+  workflow_status="Failure"
+fi
+
+send_message_to_room_response=$(curl -XPOST -d '{"msgtype": "m.text","body": "", "format": "org.matrix.custom.html","formatted_body": "<a href='https://github.com/nextcloud/integration_openproject/actions/runs/$RUN_ID'>NC-Nightly-'$BRANCH_NAME'</a><br></br><b><i>Status: '$workflow_status'</i></b>"}' "https://matrix.openproject.org/_matrix/client/r0/rooms/%21$E_ROOM_ID/send/m.room.message?access_token=$U_TOKEN")
+
+if [[ "$send_message_to_room_response" != *"event_id"* ]]; then
+  log_error "Failed to send message to element. Below response did not contain event_id!"
+  log_info "$send_message_to_room_response"
+  exit 1
+fi
+
+log_success "Notification of the nightly build has been sent to Element chat (OpenProject + Nextcloud)"
